f1aaac267ccb5815737ce6cb2121b4d13c7f6b37
Ali Akin <aliakn.20@gmail.com>
2023-09-02 19:00:38 +0300

    DAL added

---
+++ DataAccessLayer/Concrete/PersonelConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class PersonelConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            var newUser = new AppUser()
+            {
+                Id = 1,
+                Email = "aliakin@gmail.com",
+                EmailConfirmed = true,
+                Name = "Ali",
+                Surname = "Akin",
+                UserName = "aliakin",
+                NormalizedUserName = "ALIAKIN",
+            };
+
+            PasswordHasher<AppUser> newPass = new PasswordHasher<AppUser>();
+            newUser.PasswordHash = newPass.HashPassword(newUser, "123456789*Aa");
+
+            builder.HasData(newUser);
+        }
+    }
+}
---
+++ DataAccessLayer/Migrations/20230725123428_mig_add_contactus.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_add_contactus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactsUses",
+                columns: table => new
+                {
+                    ContactUsID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageBody = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MessageStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactsUses", x => x.ContactUsID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactsUses");
+        }
+    }
+}
